e96d9b301cd13b00a5f61a96f8f105156b5f715b
Nivya98 <nivya@urbanmorph.com>
2024-08-14 10:20:03 +0530

    Many to Many relation UserStock table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,15 +4,25 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:ticker])
       if @stock 
         render 'users/my_portfolio'
+        #respond_to do |format|
+        #  format.js { render partial: 'users/result' }
+        #  format.html { render partial: 'users/result' }
+        #end     
       else
-        flash[:alert] = "Stock not found. Please check the ticker symbol and try again with a valid symbol."
+        flash.now[:alert] = "Stock not found. Please check the ticker symbol and try again with a valid symbol." 
         redirect_to my_portfolio_path 
-      end
+        #respond_to do |format|
+         # flash.now[:alert] = "Stock not found. Please check the ticker symbol and try again with a valid symbol."
+         # format.js { render partial: 'users/result' }
+        #end
+      end 
     else
-      flash[:alert] = "Please enter a symbol to search"
+      flash.now[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path 
-    end
-      
-  end
-  
+      #respond_to do |format|
+       # flash.now[:alert] = "Please eneter a valid symbol to search."
+       # format.js { render partial: 'users/result' }
+      #end 
+    end  
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks
  
   root 'home#index'
   get 'my_portfolio',to: 'users#my_portfolio'
